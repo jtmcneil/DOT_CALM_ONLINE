b8290d46abeeaab26e1d75cd81931dfbdef3b5e5
jtmcneil <jaxon.t.mcneil@gmail.com>
2026-01-04 22:09:56 -0700

    updated home page links

--- src/components/news/News.ts
+++ src/components/news/News.ts
@@ -6,22 +6,22 @@ export interface Article {
 export const news: Article[] = [
     {
         subject: `STOP! DROP! ROLL! NOW!`,
-        body: `[You the flyest one](https://open.spotify.com/track/5IHnIrtg9kQHFUasNFatfl?si=c380fecec1c94659) when you walk up inside the building...`,
+        body: `[You the flyest one](https://dotttttt.com/music/stopdroproll) when you walk up inside the building...`,
     },
     {
         subject: `NEW SINGLE! GO DJ!`,
-        body: `GO DJ! GO DJ! GO DJ! GO DJ! GO DJ! GO DJ! [GO DJ!](https://open.spotify.com/track/7jNZzjN2AXsPcDAqICiOe3?si=0a6d7f6d8cba4d43) GO DJ! GO DJ! GO DJ! GO DJ! GO DJ!`,
+        body: `GO DJ! GO DJ! GO DJ! GO DJ! GO DJ! GO DJ! [GO DJ!](https://dotttttt.com/music/godj) GO DJ! GO DJ! GO DJ! GO DJ! GO DJ!`,
     },
     {
         subject: `BYTE SIZE OUT NOW!`,
-        body: `8 bits === 1 byte. Listen here: [BYTE SIZE](https://open.spotify.com/album/0oEsRH85rANeBBcHV7b4o8?si=BtFgjecLTxC9wDVIZMsVQw)`,
+        body: `8 bits === 1 byte. Listen here: [BYTE SIZE](https://dotttttt.com/music/bytesize)`,
     },
     {
         subject: `BACK//SPACE`,
-        body: `[PLEASE DON'T QUESTION ME IF I GO MIA](https://open.spotify.com/track/3XQBe56ZOe6gv1T0vl5D2k?si=1ed8304ddad040ae)`,
+        body: `[PLEASE DON'T QUESTION ME IF I GO MIA](https://dotttttt.com/music/backspace)`,
     },
     {
         subject: `a bit TOO much`,
-        body: `[0b00000001](https://open.spotify.com/track/31vomdvNxK8JSpzAIvEvLF?si=86ad227f68004e89)`,
+        body: `[0b00000001](https://dotttttt.com/music/abit2much)`,
     },
 ];
